8c4977aface976a7aef42a0154b8f9393bbbd4cd
GitPS <sime.phil@uwlax.edu>
2013-10-16 17:13:42 -0500

    Add skeleton files.

---
+++ bounded_buffer.c
@@ -0,0 +1,9 @@
+/*
+ * Ryan Gittins and Phillip Sime
+ * 
+ * CS441: Project 2 Part 1
+ * 2013-10-16
+ * 
+ */
+
+#include "bounded_buffer.h"
---
+++ bounded_buffer.h
@@ -0,0 +1,7 @@
+/*
+ * Ryan Gittins and Phillip Sime
+ * 
+ * CS441: Project 2 Part 1
+ * 2013-10-16
+ * 
+ */
